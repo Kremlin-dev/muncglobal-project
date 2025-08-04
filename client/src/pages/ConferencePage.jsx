160c8373f9cfefd60995576f218b02b80a755e8c
Kremlin-dev <yawamp27@gmail.com>
2025-08-04 22:49:23 +0000

    well

--- client/src/pages/ConferencePage.jsx
+++ client/src/pages/ConferencePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import CountdownContainer from '../components/Common/CountdownContainer';
+import CountdownContainer from '../components/common/CountdownContainer';
 
 const ConferencePage = () => {
   // Animation variants
